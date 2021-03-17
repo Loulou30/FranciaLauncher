560fdfc9ec511c262e0d7ae2c2f73b5dda4ac85f
L-C-B <74789396+L-C-B@users.noreply.github.com>
2021-03-17 14:42:34 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const { app, ipcMain, BrowserWindow } = require("electron");
+const { app, ipcMain, BrowserWindow} = require("electron");
 const path = require("path");
 const { Client, Authenticator } = require("minecraft-launcher-core");
-const {autoUpdate} = require('electron-auto-update');
- 
-autoUpdate();
-
+const Launcher = new Client();
+const autoUpdater = require('electron-updater').autoUpdater
+function UpdateAuto() {
+autoUpdater.checkForUpdates();
+autoUpdater.downloadUpdate();
+}
 let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -24,50 +26,68 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadURL(path.join(__dirname, "assets/app/login.html"));
+  mainWindow.loadFile(path.join(__dirname, "assets/app/login.html"));
 }
 app.whenReady().then(() => {
   createWindow();
-
-  app.on("activate", function () {
+app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
-ipcMain.on("login", (evt, data) =>{
-  Authenticator.getAuth(data.user,data.pass)
-  .then((user) => {
-    mainWindow.loadURL(path.join(__dirname, "assets/app/app.html")).then(() => {
-      mainWindow.webContents.send("user", user);
-      mainWindow.webContents.send("user", user.uuid);
-
-    });
-  }).catch(() => {
-    evt.sender.send("err", "Identifiants invalides");
-  });
+ipcMain.on('login',(evt,data)=>{
+  Authenticator.getAuth(data.user, data.pass)
+    .then((user) => {
+      mainWindow.loadFile(path.join(__dirname, 'assets/app/app.html')).then(() => {
+        mainWindow.webContents.send('user', user);
+        console.log('Pseudo : ' + user.name)
+      })
+    })
+  .catch(() => {
+  evt.sender.send('err', 'Mauvais identifiants');
+ });
 });
-ipcMain.on("loginToken", (evt, data) =>{
-  Authenticator.getAuth(data.access_token, data.client_token)
-  .then((user) => {
-    mainWindow.loadURL(path.join(__dirname, "assets/app/app.html")).then(() => {
-      mainWindow.webContents.send("user", user)
-      mainWindow.webContents.send("user", user.uuid);
-    
+ipcMain.on('loginToken', (evt, data) => {
+  Authenticator.refreshAuth(data.access_token, data.client_token)
+    .then((user) => {
+      mainWindow.loadFile(path.join(__dirname, 'assets/app/app.html')).then(() => {
+        mainWindow.webContents.send('user', user);
+        console.log('Pseudo : ' + user.name)
+      });
+    })
+    .catch(() => {
+      evt.sender.send('err', 'Tokens expirés');
     });
- 
-  }).catch(() => {
-    evt.sender.send("err", "Token expiré");
-  });
 });
+ipcMain.on('play', (evt, data )=> {
+  let options = {
+    clientPackage: null,
+    authorization: Authenticator.refreshAuth(data.access_token, data.client_token),
+    root: `C:/Users/${process.env.username || process.env.user}/AppData/Roaming/.spectreclient`,
+    version: {
+        number: "1.8.9",
+        type: "release"
+    },
+    memory: {
+        max: "2G",
+        min: "1G"
+    },
+    window: {
+      width: "854",
+      height: "480"
+    },
+}
+Launcher.launch(options).catch((err) => {
+evt.sender.send('err', 'Erreur lors du lancement de Minecraft')
+app.quit()
+});
+Launcher.on('debug', (e) => console.log(e))
+Launcher.on('data', (e) => console.log(e));    
+})
 ipcMain.on('logout', (evt, user) => {
-  mainWindow.loadURL(path.join(__dirname, 'assets/app/login.html')).then(() => {
-  }).catch(() => {
-      evt.sender.send("err", "Erreur lors de la déconnexion");
-    });
+  mainWindow.loadFile(path.join(__dirname, 'assets/app/login.html'))
+    .catch(() => {});
+    evt.sender.send('err', 'Erreur lors de la déconnexion')
   });
-
-
-
-
